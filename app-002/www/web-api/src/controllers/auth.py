3c4e4a3d944898a5b315c82529b12d94f1c579ab
santiaago <santiago.ariassar@gmail.com>
2018-05-13 16:34:15 +0100

    store users in cache

--- app-002/www/web-api/src/controllers/auth.py
+++ app-002/www/web-api/src/controllers/auth.py
@@ -1,11 +1,9 @@
 'authentication controller'
 
-import logging
-
 from flask import redirect
 from flask import jsonify
 from flask import flash
-from flask import session
+from flask import current_app
 
 from flask_login import login_required
 from flask_login import current_user
@@ -19,9 +17,6 @@ from flask_dance.contrib.facebook import facebook
 from authentication.whitelist import is_in_whitelist
 from authentication.oauth import OAuthSignIn
 from authentication.NotAuthorizedException import NotAuthorizedException
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 class AppUser(UserMixin): 
     def __init__(self, id, username):
@@ -39,23 +34,31 @@ def init_auth_controller(app, login_manager):
         client_id=fb_key,
         client_secret=fb_secret,
         scope=['email'],
-        redirect_url="/callback/facebook"
+        redirect_url='/callback/facebook'
     )
 
-    app.register_blueprint(facebook_bp, url_prefix="/login")
+    app.register_blueprint(facebook_bp, url_prefix='/login')
 
 
     @login_manager.user_loader
     def load_user(id):
-        database = session['db']
+        current_app.logger.info('loading user %s', id)
+        
+        database = current_app.cache.get('db')
+        if database is None:
+            current_app.logger.error('unable to retrieve database')
+            raise Exception('database not found')
         users = database.get_user(id)
         user = None
-        if users is not None and users.count() != 0:
+        if users is not None and len(users) != 0:
+            current_app.logger.info('user found')
             db_user = users[0]
             user = AppUser(
-                db_user['fb_uid'],
-                db_user['username']
+                db_user.id,
+                db_user.username
             )
+        else:
+            current_app.logger.info('user not found in database')
         return user
 
     @app.route('/logout')
@@ -66,6 +69,7 @@ def init_auth_controller(app, login_manager):
 
     @app.route('/callback/<provider>')
     def callback(provider):
+        current_app.logger.info('callback/provider')
         if not current_user.is_anonymous:
             return redirect('/')
         oauth = OAuthSignIn.get_provider(provider)
@@ -74,44 +78,37 @@ def init_auth_controller(app, login_manager):
             flash('Authentication failed.')
             return redirect('/')
 
-        resp = facebook.get("/me?fields=id,name,email,picture")
+        resp = facebook.get('/me?fields=id,name,email,picture')
 
         def dump(obj):
             for attr in dir(obj):
-                logger.info("obj.%s = %r" % (attr, getattr(obj, attr)))
+                current_app.logger.info('obj.%s = %r' % (attr, getattr(obj, attr)))
         dump(resp)
-        logger.info(social_id)
-        logger.info(username)
-        logger.info(email)
+        current_app.logger.info(social_id)
+        current_app.logger.info(username)
+        current_app.logger.info(email)
         
         if not is_in_whitelist(email):
             raise NotAuthorizedException(username)
 
         user = None
-        database = session['db']
+
+        database = current_app.cache.get('db')
         users = database.get_user(social_id)
-        if users is not None and users.count() != 0:
+        if users is not None and len(users) != 0:
             user = users[0]
 
         if not user:
-            app.logger.info('create the user and insert it into the database.')
-            app.logger.info('profile: %s\n%s\n%s\n',
+            current_app.logger.info('create the user and insert it into the database.')
+            current_app.logger.info('profile: username:%s\nsocial_id:%s\n',
                             username,
-                            social_id,
-                            '')
+                            social_id)
 
-            user = {
-                'username': username,
-                'fb_uid': social_id,
-                'fb_access_token': ''
-            }
+            user = AppUser(social_id, username)
 
+            current_app.logger.info('updating cache with user')
             database.insert_user(user)
-
-        # update fb access token to null
-        elif len(user['fb_access_token']) > 0:
-            app.logger.info('existing user, update the access token to null')
-            user['fb_access_token'] = ''
+            current_app.cache.set('db', database)
 
         login_user(AppUser(social_id, username), True)
         return redirect('/')
@@ -120,13 +117,21 @@ def init_auth_controller(app, login_manager):
     @login_required
     def get_username():
         try:
+            current_app.logger.info('auth.get_username')
             oauth = OAuthSignIn.get_provider('facebook')
             social_id, username, email = oauth.callback()
-            resp = facebook.get("/me?fields=id,name,email,picture")
+            current_app.logger.info('from callback id: %s username: %s email: %s',
+                social_id,
+                username,
+                email)
+            
+            resp = facebook.get('/me?fields=id,name,email,picture')
 
             def dump(obj):
+                'dump object attributes'
                 for attr in dir(obj):
-                    logger.info("obj.%s = %r" % (attr, getattr(obj, attr)))
+                    current_app.logger.info('obj.%s = %r' % (attr, getattr(obj, attr)))
+
             dump(resp)
 
             if resp.status_code != 200:
@@ -134,5 +139,5 @@ def init_auth_controller(app, login_manager):
                 return 'Unauthorized', 401
             return jsonify({'username': username})
         except Exception as ex:
-            logger.info('failed to get username for current user %s', ex)
+            current_app.logger.info('failed to get username for current user %s', ex)
             raise ex
